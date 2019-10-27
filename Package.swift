32848a3d1c3ff04f61fd96e23ce430218f0cbc03
Jeffrey MACKO <github.jm@macko.fr>
2019-10-27 23:58:22 +0100

    try fix

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
         .library(name: "Swagger", targets: ["Swagger"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.8.0"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.2"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
-        .package(url: "https://github.com/yonaskolb/Stencil.git", from: "0.9.0"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.0"),
-        .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "3.3.0"),
-        .package(url: "https://github.com/kylef/Spectre.git", from: "0.7.0"),
+        .package(url: "https://github.com/mackoj/Stencil.git", from: "0.9.4"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.1"),
+        .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "4.1.0"),
+        .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.0"),
     ],
     targets: [
